0c524906962221f2de3ac0eda1e4ca2ec353a7e2
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2024-03-26 00:08:59 +0100

    Update TweenLibrary.lua
    better

--- TweenLibrary.lua
+++ TweenLibrary.lua
@@ -1,7 +1,9 @@
 -- Roblox (Krampus)
 -- Fully written from scratch by Asgerpasker, literally anyone could make this anyway
--- Could be made better, just wanted it to look more nice
--- Unsafe code (no checks and error debug), meant to be used on developer side not user side
+-- Could be made better, just wanted it to look a little bit more readable
+-- Unsafe code (no checks and error debug), meant to be used on developer side not user side blah blah blah I am just saying stuff
+
+-- Add tween methods (Quad, Linear)
 
 local RunService = game:GetService("RunService");
 local CLAMP, INSERT, REMOVE = math.clamp, table.insert, table.remove;
@@ -12,7 +14,7 @@ RunService:BindToRenderStep("TweenLoop", 200, function() -- Give least priority 
         local CurrentTime = tick(); -- best like thuis
 
         for i,v in pairs(CurrentTweens) do
-            -- Don't wanna bother with many variables
+            -- Cancer math
             local Elapsed = CurrentTime - v.StartTime;
             v.CurrentValue = v.StartValue + (v.EndValue - v.StartValue) * CLAMP(Elapsed / v.Duration, 0, 1); -- Redo maths to be faster and more performance friendly if possible
 
@@ -21,13 +23,11 @@ RunService:BindToRenderStep("TweenLoop", 200, function() -- Give least priority 
             end);
 
             if v.CurrentValue == v.EndValue then
-                REMOVE(CurrentTweens, i);
-
                 if v.OnEnd then
-                    task.spawn(function() -- needed since there's probablæy other things in the table
-                        v.OnEnd(); -- No need to return value since OnChange also fires anyway
-                    end);
+                    task.spawn(v.OnEnd); -- Don't wanna fire with current value since onchanged fires on last value anyway so people can just use that
                 end;
+
+                REMOVE(CurrentTweens, i); -- Remove tween stuff from table since it's done anyway
             end;
 
             -- TableLength, TableLoop, TweenLoop
@@ -36,25 +36,25 @@ RunService:BindToRenderStep("TweenLoop", 200, function() -- Give least priority 
 end);
 -- End (YIKES)
 
-function Tween(info) -- Unsafe code (no checks)
+function Tween(info)
     local TweenInfo = {
         StartTime = tick(),
         Duration = info.Duration or 1,
 
-        OnChange = info.OnChange, -- function
-        OnEnd = info.OnEnd, -- function
+        OnChange = info.OnChange, -- func
+        OnEnd = info.OnEnd, -- func
 
         StartValue = info.StartValue or 0,
         CurrentValue = info.StartValue or 0, -- Maybe dont set manually like this and in the loop? prob don't matter
         EndValue = info.EndValue or 1,
-    }; INSERT(CurrentTweens, TweenInfo); -- Makes it just look a little nicerr
+    }; INSERT(CurrentTweens, TweenInfo);
 end; getgenv().Tween = Tween;
 
 return Tween;
 
--- Example (Tested in baseplate game)
+-- Example
 --[[
-local Tween = loadstring(game:HttpGet("https://raw.githubusercontent.com/Asgerpasker/Public/main/TweenLibrary.lua"))(); -- Don't really need to declare it since it's added to the global env (getgenv)
+local Tween = loadstring(game:HttpGet("https://raw.githubusercontent.com/Asgerpasker/Public/main/TweenLibrary.lua"))(); -- Gets added to global exploit env anyway, probably doesn't matter much
 local Baseplate = game.Workspace.Baseplate;
 
 Tween({
