c9f825564850f2844411ee7178d898eb5c3a8f5a
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2024-05-16 22:23:18 +0200

    Update TweenLibrary.lua
    summarisedddddddddddddddddddddddddddddddddddddddddddddd

--- TweenLibrary.lua
+++ TweenLibrary.lua
@@ -1,56 +1,54 @@
--- Roblox (Krampus)
--- Fully written from scratch by Asgerpasker, literally anyone could make this anyway
--- Could be made better, just wanted it to look a little bit more readable
--- Unsafe code (no checks and error debug), meant to be used on developer side not user side blah blah blah I am just saying stuff
+-- Add tween methods later
 
--- Add tween methods (Quad, Linear)
+local White = Color3.fromRGB(255, 255, 255); -- just so white stays white
 
 local RunService = game:GetService("RunService");
 local CLAMP, INSERT, REMOVE = math.clamp, table.insert, table.remove;
+local SPAWN, DummyFunc = task.spawn, function() end;
 local CurrentTweens = {}; -- table.create(10)
 
-RunService:BindToRenderStep("TweenLoop", 200, function() -- Give least priority since it's just valuez
-    if #CurrentTweens > 0 then
-        local CurrentTime = tick(); -- best like thuis
+SPAWN(function()
+    while RunService.Heartbeat:Wait() do -- inf loop
+        if #CurrentTweens == 0 then
+            continue;
+        end;
+        local CurrentTime = tick();
 
         for i,v in pairs(CurrentTweens) do
-            -- Cancer math
             local Elapsed = CurrentTime - v.StartTime;
-            v.CurrentValue = v.StartValue + (v.EndValue - v.StartValue) * CLAMP(Elapsed / v.Duration, 0, 1); -- Redo maths to be faster and more performance friendly if possible
+            v.CurrentValue = v.StartValue + (v.EndValue - v.StartValue) * CLAMP(Elapsed / v.Duration, 0, 1);
 
-            task.spawn(function() -- eh
+            SPAWN(function() -- eh
                 v.OnChange(v.CurrentValue);
             end);
-
+        
             if v.CurrentValue == v.EndValue then
-                if v.OnEnd then
-                    task.spawn(v.OnEnd); -- Don't wanna fire with current value since onchanged fires on last value anyway so people can just use that
-                end;
-
-                REMOVE(CurrentTweens, i); -- Remove tween stuff from table since it's done anyway
+                REMOVE(CurrentTweens, i);
+                v.OnEnd();
             end;
-
-            -- TableLength, TableLoop, TweenLoop
         end;
     end;
 end);
--- End (YIKES)
 
 function Tween(info)
-    local TweenInfo = {
+    INSERT(CurrentTweens, {
         StartTime = tick(),
         Duration = info.Duration or 1,
 
-        OnChange = info.OnChange, -- func
-        OnEnd = info.OnEnd, -- func
+        OnChange = info.OnChange or DummyFunc,
+        OnEnd = info.OnEnd or DummyFunc,
 
         StartValue = info.StartValue or 0,
-        CurrentValue = info.StartValue or 0, -- Maybe dont set manually like this and in the loop? prob don't matter
+        CurrentValue = info.StartValue or 0,
         EndValue = info.EndValue or 1,
-    }; INSERT(CurrentTweens, TweenInfo);
-end; getgenv().Tween = Tween;
+    });
+end;
 
-return Tween;
+if getgenv and typeof(getgenv()) == "table" then
+    getgenv().Tween = Tween;
+end;
+
+return Tween; -- Doesn't matter, since I am adding it to the global env (getgenv) anyway
 
 -- Example
 --[[
