0d51b9b339eda0ea33d3f59c42020d529f51487d
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2024-10-23 22:25:39 +0200

    Update UnloadableHookMetaMethod.lua

--- UnloadableHookMetaMethod.lua
+++ UnloadableHookMetaMethod.lua
@@ -1,55 +1,59 @@
 if not hookmetamethod then
-  return warn "shitsploit get money brokie";
+	return warn "shitsploit get money brokie";
 elseif HookedMetaMethods then
-  return;
+	return;
 end;
 local isreadonly, setreadonly, getrawmt, OldHookmetamethod = isreadonly, setreadonly, getrawmetatable, hookmetamethod;
-local INSERT, CLEAR, SUB = table.insert, table.clear, string.sub;
+local INSERT, CLEAR, REMOVE, SUB = table.insert, table.clear, table.remove, string.sub;
 
 local HookedMetaMethods;
 HookedMetaMethods = {
-    Revert = function(tbl)
-        tbl.RawMetatable[tbl.Method] = tbl.OldMethod
-        setreadonly(tbl.RawMetatable, tbl.ReadOnly);
-        CLEAR(tbl);
-    end,
+	Revert = function(tbl)
+		for i,v in next, tbl do
+		end;
+		tbl.RawMetatable[tbl.Method] = tbl.OldMethod;
+		setreadonly(tbl.RawMetatable, tbl.ReadOnly);
+		CLEAR(tbl);
+	end,
 
-    Unload = function(oldmethod)
-        if oldmethod then
-            for i,v in next, HookedMetaMethods do
-                if v.OldMethod == oldmethod then
-                    HookedMetaMethods.Revert(v);
-                    return; -- exit loop and func
-                end;
-            end;
-        end;
+	Unload = function(oldmethod)
+		if oldmethod then
+			for i,v in next, HookedMetaMethods do
+				if v.OldMethod == oldmethod then
+					HookedMetaMethods.Revert(v);
+					return; -- exit loop and func
+				end;
+			end;
+		end;
 
-        for i,v in next, HookedMetaMethods do
-            HookedMetaMethods.Revert(v);
-        end;
-        CLEAR(HookedMetaMethods);
-    end,
+		for i,v in next, HookedMetaMethods do
+			if typeof(v) == "table" then
+				HookedMetaMethods.Revert(v);
+				REMOVE(HookedMetaMethods, i);
+			end;
+		end;
+	end,
 };
 
 getgenv().HookedMetaMethods = HookedMetaMethods;
 
 getgenv().hookmetamethod = function(inst, method, hook)
-    if SUB(method, 1, 2) == "__" then
-      warn("Using old hookmetamethod due to method containg __ in start, method: "..method..", inst / object: "..inst);
-      return OldHookmetamethod(inst, method, hook);
-  end;
-  
-    local RawMetatable, Method = getrawmt(inst), "__"..method;
-    local OldMethod, ReadOnly = RawMetatable[Method], isreadonly(RawMetatable);
-    setreadonly(RawMetatable, false);
+	if SUB(method, 1, 2) == "__" then
+		warn("Using old hookmetamethod due to method containg __ in start, method: "..method..", inst / object: "..inst);
+		return OldHookmetamethod(inst, method, hook);
+	end;
 
-    INSERT(HookedMetaMethods,  {
-        RawMetatable = RawMetatable,
-        Method = Method,
-        OldMethod = OldMethod,
-        ReadOnly = ReadOnly,
-    });
+	local RawMetatable, Method = getrawmt(inst), "__"..method;
+	local OldMethod, ReadOnly = RawMetatable[Method], isreadonly(RawMetatable);
+	setreadonly(RawMetatable, false);
 
-    RawMetatable[Method] = hook;
-    return OldMethod;
+	INSERT(HookedMetaMethods,  {
+		RawMetatable = RawMetatable,
+		Method = Method,
+		OldMethod = OldMethod,
+		ReadOnly = ReadOnly,
+	});
+
+	RawMetatable[Method] = hook;
+	return OldMethod;
 end;
