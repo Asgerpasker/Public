3845ce18f72844f0068de52a11f2121c755d80cc
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2024-10-30 21:38:35 +0100

    Update TweenLibrary.lua

--- TweenLibrary.lua
+++ TweenLibrary.lua
@@ -1,63 +1,55 @@
--- Add tween methods later (no? too gay math)
--- micro optimz
+local MaxCache = 50;
 
--- Just in case white should stay white
-local White = Color3.new(1, 1, 1);
+local TweenService = game:GetService("TweenService");
+local TweenCreate, TWINFO, LINEAR = TweenService.Create, TweenInfo.new, Enum.EasingStyle.Linear;
+local CLEAR, REMOVE, INSERT, INST = table.clear, table.remove, table.insert, Instance.new;
+local PlaceHolder = function()end;
+local wait, next = task.wait, next;
 
-local RunService = game:GetService("RunService");
-local CLAMP, MIN = math.clamp, math.min;
-local INSERT, REMOVE, CLEAR = table.insert, table.remove, table.clear;
-local SPWN = task.spawn; -- cache it cuz
-local CurrentTweens = {};
+local FolderCache = INST("Folder", game:GetService("CoreGui"));
+FolderCache.Name = "TweensCache";
 
-function SPAWN(func, ...)
-	if func then
-		SPWN(func, ...);
-	end;
+-- Cache for speed
+local Cache = {};
+for i = 1, MaxCache do
+    INSERT(Cache, INST("NumberValue", FolderCache));
 end;
 
-RunService:BindToRenderStep("__TweenLoopLinear", 199, function()
-	local CurrentTime = tick();
-
-	for i,v in next, CurrentTweens do
-		local Multiplier = MIN((CurrentTime - v.StartTime) / v.Duration, 1);
-		SPAWN(v.OnChanged, v.StartValue + v.Delta * Multiplier);
-
-		if Multiplier == 1 then	
-			SPAWN(v.OnFinished, v.StartValue + v.Delta * Multiplier); -- caus
-			CLEAR(v); -- This might be retarded but i aint sure how good the gc is for cleang up unused tables (i aint reading the manual)
-			REMOVE(CurrentTweens, i);
-		end;
-	end;
+-- Clean up cache just cuz (add simple algo in future to adjust maxsize)
+task.spawn(function()
+    while wait(10) do
+        for i,v in next, Cache do
+            if i > MaxCache then
+                REMOVE(Cache, i);
+                v:Destroy();
+            end;
+        end;
+    end;
 end);
+--
 
-function Tween(info)
-	INSERT(CurrentTweens, {
-		StartTime = tick(),
-		Duration = info.Duration,
-		StartValue = info.StartValue,
-		Delta = info.EndValue - info.StartValue,
+local function Tween(info)
+    local End, OnChanged, OnFinished = info.End or PlaceHolder, info.OnChanged, info.OnFinished or PlaceHolder;
+    if not Cache[1] then
+        INSERT(Cache, INST("NumberValue"));
+    end;
 
-		OnChanged = info.OnChanged,
-		OnFinished = info.OnFinished,
-	});
+    local Number = Cache[1];
+    Number.Value = info.From;
+    REMOVE(Cache, 1);
+    
+    local Connection;
+    Connection = Number:GetPropertyChangedSignal("Value"):Connect(function()
+        if Number.Value == End then
+            Connection:Disconnect();
+            INSERT(Cache, Number); CLEAR(info);
+            OnFinished(End);
+        else
+            OnChanged(Number.Value);
+        end;
+    end);
+
+    TweenCreate(TweenService, Number, TWINFO(info.Duration, LINEAR), {Value = End}):Play();
 end;
 
-
-(getgenv and getgenv() or _G).Tween = Tween;
-return Tween; -- Doesn't really matter, since it should be added to the global env anyway
-
---[[
--- Example
-
-local Tween = loadstring(game:HttpGet("https://raw.githubusercontent.com/Asgerpasker/Public/main/TweenLibrary.lua"))(); -- looks nicer
-Tween({
-	Duration = 1,
-	StartValue = 0,
-	EndValue = 1,
-	
-	OnChanged = function(value)
-		game.Players.LocalPlayer.Character.UpperTorso.Transparency = value;
-	end,
-});
---]]
+return Tween; -- why eevn add to global env if we dont call from there
