62fdae5d752375f6a2f8244edab574d21d5e8c28
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2024-12-05 22:49:29 +0100

    Update TweenLibrary.lua

--- TweenLibrary.lua
+++ TweenLibrary.lua
@@ -1,35 +1,37 @@
-local Tweens = table.create(50); -- prob doesn't do shit
+local Interval = 1 / 60; -- .016
+
+local Tweens = table.create(500); -- prob doesn't do shit
 local INSERT, CLEAR, REMOVE = table.insert, table.clear, table.remove;
-local spawn, tick = task.spawn, tick;
+local spawn, wait, tick = task.spawn, task.wait, tick;
 local MIN = math.min;
 
-function Tween(info)
+local function Tween(info)
 	INSERT(Tweens, {
 		Start = tick(), Duration = info.Duration,
-		From = info.From, To = info.To,
-		Delta = info.To - info.From,
+		From = info.From, Delta = info.To - info.From,
 		OnChanged = info.OnChanged, OnFinished = info.OnFinished,
 	});
 end;
 
-game:GetService("RunService").RenderStepped:Connect(function()
-	local CurrentTick = tick();
+spawn(function()
+    while wait(Interval) do
+        local CurrentTick = tick();
 
-	for i,v in Tweens do
-		local Multiplier = MIN((CurrentTick - v.Start) / v.Duration, 1);
-		spawn(v.OnChanged, v.From + v.Delta * Multiplier); -- Storing delta prob doesnt do shit? i lied it does
+        for i,v in Tweens do
+            local Multiplier = MIN((CurrentTick - v.Start) / v.Duration, 1);
+            spawn(v.OnChanged, v.From + v.Delta * Multiplier); -- Storing delta prob doesnt do shit? i lied it does
 
-		if Multiplier == 1 then
-			if v.OnFinished then
-				spawn(v.OnFinished);
-			end;
+            if Multiplier == 1 then
+                if v.OnFinished then
+                    spawn(v.OnFinished);
+                end;
 
-			REMOVE(Tweens, i);
-			CLEAR(v);
-		end;
-	end;
+                REMOVE(Tweens, i);
+                CLEAR(v);
+            end;
+        end;
+    end;
 end);
-
 --[[
 Tween({
 	From = 0,
