28496d79d5c42d41dc1d25b1e87cd3da47c0c869
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2025-04-05 13:45:23 +0200

    Update TweenLibrary.lua

--- TweenLibrary.lua
+++ TweenLibrary.lua
@@ -1,6 +1,6 @@
 local Interval = 1 / 60; -- .016
 
-local Tweens = {}; -- prob doesn't do shit
+local Tweens = {};
 local INSERT, REMOVE = table.insert, table.remove;
 local spawn, wait, tick = task.spawn, task.wait, tick;
 local MIN = math.min;
@@ -14,21 +14,23 @@ local function Tween(info)
 end;
 
 spawn(function()
-    while wait(Interval) do
+    while true do
         local CurrentTick = tick();
 
         for i,v in Tweens do
             local Multiplier = MIN((CurrentTick - v.Start) / v.Duration, 1);
-            spawn(v.OnChanged, v.From + v.Delta * Multiplier); -- Storing delta prob doesnt do shit? i lied it does
+            spawn(v.OnChanged, v.From + v.Delta * Multiplier); -- lerp formula
 
             if Multiplier == 1 then
                 if v.OnFinished then
                     spawn(v.OnFinished);
                 end;
 
-                REMOVE(Tweens, i);
+		Tweens[i] = nil; -- REMOVE(Tweens, i);
             end;
         end;
+
+	wait(Interval);
     end;
 end);
 --[[
