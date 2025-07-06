197505f9ed89912303d8c50536ec74f8beaf5d3f
Asgerpasker <94760199+Asgerpasker@users.noreply.github.com>
2025-07-07 00:10:55 +0200

    Update TweenLibrary.lua

--- TweenLibrary.lua
+++ TweenLibrary.lua
@@ -1,36 +1,46 @@
-local Interval = 1 / 60;
+local wait, spawn = task.wait, task.spawn;
+local GetTime = time;
 
-local Tweens = {};
-local INSERT, REMOVE, MIN = table.insert, table.remove, math.min;
-local spawn, wait, tick = task.spawn, task.wait, tick;
+local InitTween = function(tween)
+    local From = tween.From;
+    local Duration = tween.Duration;
+    local OnChanged = tween.OnChanged;
 
-local function Tween(info)
-	INSERT(Tweens, {
-		Start = tick(), Duration = info.Duration,
-		From = info.From, Delta = info.To - info.From,
-		OnChanged = info.OnChanged, OnFinished = info.OnFinished,
-	});
-end;
-
-spawn(function()
+    local Start = GetTime();
+    local Delta = tween.To - From;
     while true do
-        local CurrentTick = tick();
+        local Multiplier = (GetTime() - Start) / Duration;
 
-        for i,v in Tweens do
-            local Multiplier = MIN((CurrentTick - v.Start) / v.Duration, 1);
-            spawn(v.OnChanged, v.From + v.Delta * Multiplier); -- lerp formula
-
-            if Multiplier == 1 then
-                if v.OnFinished then
-                    spawn(v.OnFinished);
-                end;
-
-		REMOVE(Tweens, i);
-            end;
+        if Multiplier > 1 then
+            tween.OnFinished();
+            break;
+        else
+            OnChanged(From + Delta * Multiplier);
         end;
 
-	wait(Interval);
+        wait(1/60); -- should be optimized to its result by compiler
     end;
-end);
+end;
+
+local Tween = function(tween)
+    -- basically just a wrapper but it looks nicer
+    spawn(InitTween, tween);
+end;
+
+--[[
+Tween({
+    From = 0,
+    To = -5,
+    Duration = 5,
+
+    OnChanged = function(val)
+        print("value", val);
+    end,
+
+    OnFinished = function()
+        print "done";
+    end;
+});
+]]
 
 return Tween;
